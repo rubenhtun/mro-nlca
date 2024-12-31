1d6882c1e4486a2d56e7a365d71206e488da4a43
RubenHtun <rubenhtun2003@gmail.com>
2024-12-31 16:06:27 +0700

    updated

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,28 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.getElementById("menuButton");
-  const mobileMenu = document.getElementById("mobileMenu");
-  const mobileAboutButton = document.getElementById("mobileAboutButton");
-  const mobileAboutMenu = document.getElementById("mobileAboutMenu");
+const menuButton = document.getElementById("menuButton");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileAboutButton = document.getElementById("mobileAboutButton");
+const mobileAboutMenu = document.getElementById("mobileAboutMenu");
 
-  // Toggle mobile menu
-  menuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("open");
-    const icon = menuButton.querySelector("i");
-    if (mobileMenu.classList.contains("open")) {
-      icon.classList.remove("fa-bars");
-      icon.classList.add("fa-xmark");
-    } else {
-      icon.classList.remove("fa-xmark");
-      icon.classList.add("fa-bars");
-    }
-  });
+// Toggle mobile menu
+menuButton.addEventListener("click", () => {
+  const menuIcon = menuButton.querySelector("i");
+  if (mobileMenu.classList.contains("-translate-y-full")) {
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+    mobileMenu.classList.remove("-translate-y-full", "opacity-0");
+    mobileMenu.classList.add("translate-y-0", "opacity-100");
+  } else {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+    mobileMenu.classList.remove("translate-y-0", "opacity-100");
+    mobileMenu.classList.add("-translate-y-full", "opacity-0");
+  }
+});
 
-  // Toggle About submenu
-  mobileAboutButton.addEventListener("click", function () {
-    mobileAboutMenu.classList.toggle("open");
-    const icon = mobileAboutButton.querySelector("svg");
-    icon.style.transform = mobileAboutMenu.classList.contains("open")
-      ? "rotate(180deg)"
-      : "rotate(0)";
-  });
+// Toggle About submenu
+mobileAboutButton.addEventListener("click", function () {
+  mobileAboutMenu.classList.toggle("open");
+  const icon = mobileAboutButton.querySelector("svg");
+  icon.style.transform = mobileAboutMenu.classList.contains("open")
+    ? "rotate(180deg)"
+    : "rotate(0deg)";
 });
